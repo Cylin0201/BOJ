4afd49abb95abd5246c48e7d344d48f0a5694fa4
cylin0201 <cylin0201@naver.com>
2023-09-26 13:01:33 +0900

    update .jsons

--- ex00/hello.cpp
+++ ex00/hello.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <vector>
 
 int main()
 {
-    std::cout<<"Hello, World"<<"\n";
+    std::vector<int> v;
+    for (int i = 0 ; i < 10; i++)
+    {
+        v.push_back(i);
+    }
+    for (auto elem: v)
+        std::cout<<elem<<" ";
 }
