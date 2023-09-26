4fef8e63990d893452118f321cc208aa28d4aea5
cylin0201 <cylin0201@naver.com>
2023-09-26 13:12:31 +0900

    as

--- ex00/hello.cpp
+++ ex00/hello.cpp
@@ -6,7 +6,7 @@ int main()
     std::vector<int> v;
     for (int i = 0 ; i < 10; i++)
     {
-        v.push_back(i);
+        v.push_back(i + 1);
     }
     for (auto elem: v)
         std::cout<<elem<<" ";
